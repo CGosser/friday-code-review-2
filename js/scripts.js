e4d10dc447d25fc2a262cfa276716a95542ed94d
Chris Gosser <rivetkopf@gmail.com>
2018-08-10 09:44:07 -0700

    create variables to handle inputs

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@
 
 $(document).ready(function() {
 
+var likes = $("#likes").val();
+var where = $("#where").val();
+var movies = $("#movies").val();
+var runner = $("#runner").val();
+var vintage = $("#vintage").val();
+
+
+
 });
