436274b486ede7d0be3dc2dc49905670c623ee2a
Chris Gosser <rivetkopf@gmail.com>
2018-08-10 11:28:20 -0700

    add function which alerts user if an input field is empty

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 
 
 $(document).ready(function() {
+  // var name = $("#name")
+  var likes = $("#likes").val();
+  var where = $("#where").val();
+  var movies = $("#movies").val();
+  var runner = $("#runner").val();
+  var vintage = $("#vintage").val();
 
-var likes = $("#likes").val();
-var where = $("#where").val();
-var movies = $("#movies").val();
-var runner = $("#runner").val();
-var vintage = $("#vintage").val();
+  var resultdata = []
 
-
-
-});
+    $("#submit").click(function(){
+      if (likes == "" || where == "" || movies =="" || runner == "" || vintage ==""){
+        alert ("please answer all questions");
+      };
+      alert(likes)
+    });
+  });
